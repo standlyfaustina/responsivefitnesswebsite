2021a8b41ec4cd3c9ce398d177a513394f6c29c3
Mr Standly <standlyfaustina@gmail.com>
2021-04-14 19:13:11 +0100

    second commit

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar_menu')
 const navLogo = document.querySelector('#navbar_logo')
+const body = document.querySelector('body')
 
 //Dsiplay mobile menu
 
 const mobileMenu = () => {
-    menu.classList.toggle()
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active')
+    body.classList.toggle('active')
 }
+
+menu.addEventListener('click',mobileMenu)
