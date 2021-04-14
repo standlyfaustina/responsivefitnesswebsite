b8b578dc6567f619dedf21ecb22c49619ce9527b
Mr Standly <standlyfaustina@gmail.com>
2021-04-14 14:58:56 +0100

    my first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar_menu')
+const navLogo = document.querySelector('#navbar_logo')
+
+//Dsiplay mobile menu
+
+const mobileMenu = () => {
+    menu.classList.toggle()
+}
